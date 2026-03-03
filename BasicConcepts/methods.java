576832f32726d5676575d8340f7164edf2c6831b
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-03 21:48:21 +0530

    Methods Basics

--- BasicConcepts/methods.java
+++ BasicConcepts/methods.java
@@ -2,12 +2,16 @@
 public class methods{
  
     // methods are used to perform certain actions, and they are also known as functions.
+    
     static void myMethod(int a, int b){
         int sum = a + b;
         System.out.println("The sum of a and b is: " + sum);
     }
     public static void main(String[]args){
-        myMethod(2,6);
+
+        // this function call the function.
+        myMethod(2,6); 
+        
         // Output: The sum of a and b is: 8
       
 
