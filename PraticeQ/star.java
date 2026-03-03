6d1563c204bff853617cf74df7e2fb7423c4b274
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-03 22:09:53 +0530

    star_pattrn

--- PraticeQ/star.java
+++ PraticeQ/star.java
@@ -1,12 +1,20 @@
 package PraticeQ;
 public class star{
-    public static void main(String[]args){
-        for(int i =4; i>=1; i--) {
-            for (int j = i; j >= 1; j--) {
-                System.out.print("*");
-            }
+    public static void myStar(){
+
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+            System.out.print("*");}
             System.out.println();
-
-
-        }}
-  }
+        }
+        }
+    public static void main(String[]args){
+        myStar();
+        /* OUTPUT: 
+         *
+         **
+         ***
+         ****
+         *****
+        */
+}}
