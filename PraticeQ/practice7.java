e1c27a41337d99cb85c7f1e3475211600d271174
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-03 21:52:25 +0530

    method_for_table

--- PraticeQ/practice7.java
+++ PraticeQ/practice7.java
@@ -1,9 +1,9 @@
+package PraticeQ;
 public class practice7{
   public static void multiplication(int n){
   
     for(int i=1; i<=10; i++){
       System.out.println(n+"X"+i+"="+i*n);}
-  
 
     }
   public static void main(String[]args){
