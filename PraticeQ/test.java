9bd72239f97d08642b1ba3949926a0c8dff1ebb0
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-05 21:22:19 +0530

    methods practice

---
+++ PraticeQ/test.java
@@ -0,0 +1,27 @@
+package PraticeQ;
+public class test {
+    public static void even(int a){
+        boolean iseven;
+        if(a%2==0){
+            iseven = true;
+        }else{
+            iseven = false;
+        }
+        System.out.println(" value is: "+iseven);
+        
+
+        
+    }
+    public static void main(String[]args){
+        even(3);
+
+
+      
+        
+        
+
+
+
+    }
+    
+}
