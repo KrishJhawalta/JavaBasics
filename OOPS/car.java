4acf190c3c0bd0a19410160a13c62700bfcdde65
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-06 22:30:49 +0530

    example class

---
+++ OOPS/car.java
@@ -0,0 +1,33 @@
+class car1{
+    int model;
+    String color;
+
+public void show(){
+    System.out.println("Model is: "+model);
+    System.out.println("Color is: "+color);
+}
+
+}
+
+
+
+
+public class car {
+    public static void main(String[] args) {
+
+        //Car 1
+        car1 Audi = new car1();
+        Audi.model = 2015;
+        Audi.color = "White";
+
+        // Car 2
+        car1 bmw = new car1();
+        bmw.model = 2016;
+        bmw.color = "Red";
+        
+// Printing the functions
+        Audi.show();
+        bmw.show();
+    }
+    
+}
