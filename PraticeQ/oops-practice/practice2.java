7f1aead7dc17a458288e1c17007f1b518f9d0ad9
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-07 22:42:31 +0530

    practice set

--- PraticeQ/oops-practice/practice2.java
+++ PraticeQ/oops-practice/practice2.java
@@ -1,3 +1,23 @@
-public class practice2 {
+class cellphone{
+    String Brand;
     
+    
+    
+    public void ringing(){
+        System.out.println("Ringing!");
+    }
+    
+    public void vibrate(){
+        System.out.println("Your mobile is vibrating zzz!!");
+    }
+}
+
+public class practice2{
+    public static void main(String[]args){
+        cellphone Phone = new cellphone();
+        Phone.Brand = "Apple";
+        Phone.vibrate();
+        Phone.ringing();
+        
+    }
 }
