d96f3b103e6b50cdeb1c11a5a8304a22dd6595d1
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-07 22:30:17 +0530

    question 1

--- PraticeQ/oops-practice/employee.java
+++ PraticeQ/oops-practice/employee.java
@@ -3,13 +3,13 @@ class Employee{
     String name;
     int salary;
 
-    public void getsalary(){
+    public void getsalary(){       // method to print salary
         System.out.println("Salary is: "+salary);
     }
-    public void getName(){
+    public void getName(){        // method to print name
         System.out.println("Name is: "+name);
     }
-    public void setName(String n){
+    public void setName(String n){   // method to set name
         name = n;
     }
   
@@ -17,11 +17,14 @@ class Employee{
 
 public class employee {
     public static void main(String[]args){
+
         Employee emp1 = new Employee();
         emp1.id = 101;
         emp1.name = "Harry";
         emp1.setName("Krish");
         emp1.salary = 50000;
+        
+        // printing the details of employee
         emp1.getsalary();
         emp1.getName();
 
