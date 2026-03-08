1b983fce0cd20eb2bf1c8f576d09ce06881e8753
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-08 19:49:46 +0530

    square class

--- PraticeQ/oops-practice/practice3.java
+++ PraticeQ/oops-practice/practice3.java
@@ -1,25 +1,23 @@
 class square{
     int side;
-    
-    
+
+
+
     public void area(){
         System.out.println("Area is: "+side*side);
+
     }
-    
     public void perimeter(){
-        System.out.println("perimeter is: "+4*side);
+        System.out.println("Perimeter is: "+4*side);
     }
-  
 }
 
 public class practice3{
     public static void main(String[]args){
-      square sq = new square();
-      sq.side = 5;
-    sq.area();
-    sq.perimeter();
-        
-        
-        
+        square sq = new square();
+        sq.side = 2;
+        sq.area();
+        sq.perimeter();
+
     }
 }
