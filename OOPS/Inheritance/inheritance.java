4c9ef18298acfae6f77a618856c85bdef9656618
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-13 21:35:32 +0530

    inheritance basics

--- OOPS/Inheritance/inheritance.java
+++ OOPS/Inheritance/inheritance.java
@@ -1,41 +1,27 @@
 class base{
      int x;
 
-
-    public void setX(int x) {
-        x = x;
+    public void setX(int xe) {
+        x = xe;
     }
     public int getX() {
         return x;
     }
-    public void printme(){
+    public void printMe(){
         System.out.println("I am constructor");
     }
-
 }
-class derived extends base{
+
+class derived extends base{      // extends = takes all functions of {base()} class
     int y;
-
-    public void setY(int y) {
-        this.y = y;
-    }
-    public int getY() {
-        return y;
-    }
 }
-
 public class inheritance{
 
     public static void main(String[] args) {
-        base b = new base();
-        derived d = new derived();
-        b.printme();
-        d.printme();
-        b.setX(5);
 
-        d.setY(10);
-        System.out.println("D is:"+d.getY());
-        System.out.println("Number is: "+b.getX());
+        derived d = new derived();    // derived object can access all base class { But base class cannot access derived}
+        d.setX(7);
+        System.out.println("Number is: "+d.getX());
 
 
     }
