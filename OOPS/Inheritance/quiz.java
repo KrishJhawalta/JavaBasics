e202c460ae5eee387cdfb03f864a5a028cf28531
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-13 21:36:00 +0530

    animal_dog quiz

--- OOPS/Inheritance/quiz.java
+++ OOPS/Inheritance/quiz.java
@@ -4,13 +4,16 @@ class Animal{
 
 
     void printname(){
-        System.out.println(" I Am an Animal");
+        System.out.println(" Iam an Animal");
     }
 }
 
 class dog extends Animal{
     void printFunction(){
         System.out.println(" I am dog");
+    }
+    void bark(){
+        System.out.println("Barking..");
     }
 }
 
@@ -19,6 +22,7 @@ public class quiz {
     public static void main(String[]args) {
         dog d = new dog();
         d. printFunction();
+        d.bark();
         d.printname();
 
     }
