05b46c0b860702997bf9c6f8a26298fa99072d48
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-24 22:42:32 +0530

    New pattern

---
+++ PraticeQ/StarPatterns/star1.java
@@ -0,0 +1,12 @@
+package PraticeQ.StarPatterns;
+
+public class star1 {
+    static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
