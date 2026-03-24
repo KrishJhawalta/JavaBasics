7843ac484f47761b5d5b80d126e8ca3a1c5edbeb
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-24 22:40:52 +0530

    star patterns

--- PraticeQ/funPractice/star.java
+++ PraticeQ/funPractice/star.java
@@ -1,18 +1,9 @@
 public class star {
     public static void main(String[]args){
-        for(int i=5; i>=1; i--){
-            for(int j = 5; j>=i; j--){
-                System.out.print("*");
-            }System.out.println();
-
-            /*
-            *
-            **
-            ***
-            ****
-            *****
-
-             */
+      for(int i=1; i<=10; i++){
+          for(int j=1; j<=i; j++){
+              System.out.println("*");
+          }
         }
     }
     
