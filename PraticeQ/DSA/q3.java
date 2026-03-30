a59cc55b4e2a248e6e01d9c6522eebc6914418e0
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-30 22:30:56 +0530

    new

---
+++ PraticeQ/DSA/q3.java
@@ -0,0 +1,15 @@
+package PraticeQ.DSA;
+
+public class q3 {
+    public static void main(String[] args) {
+        int num = 6;
+        int [] arr = {1,4,6,3,7};
+        for(int i =0; i<arr.length; i++){
+            if(arr[i]==num){
+                System.out.println("Element found at index: ");
+                break;
+            }
+        }
+    }
+    
+}
