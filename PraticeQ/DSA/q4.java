5a0b762c59819e3c839fd09a04aea67689f3c825
Krish Jhawalta <krishjhawalta@gmail.com>
2026-03-31 21:52:05 +0530

    q4

---
+++ PraticeQ/DSA/q4.java
@@ -0,0 +1,18 @@
+package PraticeQ.DSA;
+
+public class q4 {
+    public static void main(String[] args) {
+        
+    }
+    // sum of two numbers using functions
+
+    int sum(int a, int b){
+        return a+b;
+    }
+}
+    System.out.println(sum(2,5));
+
+   
+
+    
+
