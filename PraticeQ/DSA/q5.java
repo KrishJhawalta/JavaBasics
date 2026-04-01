79bd7b1a925e234f89e886181440c970bb76d201
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-01 21:25:58 +0530

    practice set

---
+++ PraticeQ/DSA/q5.java
@@ -0,0 +1,36 @@
+public class q5{
+
+    void calculator(int a, int b, char c){
+        switch(c){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            case '/':
+                if(b!=0){
+                    System.out.println(a/b);
+                }else{
+                    System.out.println("Cannot divide by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+
+    public static void main(String[] args) {
+        q5 obj = new q5();
+        obj.calculator(10, 5, '+');
+        obj.calculator(10, 5, '-');
+        obj.calculator(10, 5, '*');
+        obj.calculator(10, 5, '/');
+        obj.calculator(10, 0, '/');
+        obj.calculator(10, 5, '%');
+        
+    }
+}
