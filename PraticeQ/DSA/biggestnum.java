ab7126604ea7c27b3560a374f42da843f8e717c2
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-03 22:06:48 +0530

    new num

--- PraticeQ/DSA/biggestnum.java
+++ PraticeQ/DSA/biggestnum.java
@@ -3,7 +3,10 @@ package PraticeQ.DSA;
 public class biggestnum {
     public static void main() {
 
+
         int [] arr = {1,55,2,5,7,8};
+
+         // loop to run biggest number
         for(int i=0; i<arr.length; i++){
             if(arr[i]>arr[i+1]){
                 System.out.println(arr[i]);
